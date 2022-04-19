37aad8412ff7a92c69d52d4f756ef13cf8be32b5
Bhargav Srinivasan <bhargav.srinivasan92@gmail.com>
2022-04-20 00:27:31 +0530

    adding error handling

--- app.py
+++ app.py
@@ -7,27 +7,31 @@ app = Flask(__name__)
 
 @app.route('/store', methods=["GET"])
 def get_items():
-    items = controller.get_items()
-    total_price, total_tax = 0.0, 0.0
-    res = []
-    for item in items:
-        total_tax += item[4]
-        total_price += item[5]
-        res.append({
-            "item": item[1],
-            "itemCategory": item[2],
-            "price": item[3],
-            "tax": item[4],
-            "total": item[5],
-            "quantity": item[6]
-        })
+    try:
+        items = controller.get_items()
+        total_price, total_tax = 0.0, 0.0
+        res = []
+        for item in items:
+            total_tax += item[4]
+            total_price += item[5]
+            res.append({
+                "item": item[1],
+                "itemCategory": item[2],
+                "price": item[3],
+                "tax": item[4],
+                "total": item[5],
+                "quantity": item[6]
+            })
 
-    if total_price > 2000.0:
-        total_price -= (total_price*0.05)
+        if total_price > 2000.0:
+            total_price -= (total_price*0.05)
 
-    res = sorted(res, key=lambda x: x["item"])
-    res += [{"totalPrice": total_price}, {"totalTax": total_tax}]
-    return jsonify(res), 200
+        res = sorted(res, key=lambda x: x["item"])
+        res += [{"totalPrice": total_price}, {"totalTax": total_tax}]
+        return jsonify(res), 200
+    except Exception as e:
+        res = {"An exception occurred while processing the request": str(e)}
+        return jsonify(res), 500
 
 
 def validate_item(item):
@@ -50,18 +54,22 @@ def validate_item(item):
 
 @app.route("/store", methods=["POST"])
 def insert_item():
-    item_details = request.get_json()
-    result = []
-    for item in item_details:
-        if not validate_item(item):
-            return jsonify([]), 503
-        name = item["item"]
-        category = item["itemCategory"]
-        price = item["price"]
-        quantity = item["quantity"]
-        result.append( controller.insert_item(name, category, price, quantity))
+    try:
+        item_details = request.get_json()
+        result = []
+        for item in item_details:
+            if not validate_item(item):
+                return jsonify([]), 503
+            name = item["item"]
+            category = item["itemCategory"]
+            price = item["price"]
+            quantity = item["quantity"]
+            result.append( controller.insert_item(name, category, price, quantity))
 
-    return jsonify(result), 201
+        return jsonify(result), 201
+    except Exception as e:
+        res = {"An exception occured while processing the request": str(e)}
+        return jsonify(res), 500
 
 
 if __name__ == "__main__":
